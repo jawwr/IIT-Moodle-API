5350b625984b25a3a69fcfad94468e18b168e7f5
Arseny <20mineslend14@gmail.com>
2022-08-10 18:14:34 +0500

    Переделано получение всех новостей, парсинг теперь происходит каждый час

--- news service/src/main/java/com/example/newsService/service/NewsService.java
+++ news service/src/main/java/com/example/newsService/service/NewsService.java
@@ -26,21 +26,14 @@ public class NewsService {
     }
 
     public List<News> getAllNews() {
-        int count = repository.NewsCount();
-        Integer lastIdInDB = repository.findMaximumId();
-        int lastId = lastIdInDB == null ? 0 : lastIdInDB;
-        if (count != 0 && parser.checkNewNewsExist(lastId)){
-            return repository.findAll();
-        }
-        List<News> news = parser.parse(lastId);
-        saveAllNews(news);
         return repository.findAll();
     }
-    public void saveNews(News news){
+
+    public void saveNews(News news) {
         repository.save(news);
     }
 
-    public void saveAllNews(List<News> news){
+    public void saveAllNews(List<News> news) {
         repository.saveAll(news);
     }
 
@@ -51,35 +44,48 @@ public class NewsService {
         }
         return repository.findNewsAfter(id);
     }
+
     public List<News> getNewsAfterId() {
         var last = repository.findMaximumId();
         return repository.findNewsAfter(last + 1);
     }
+    @Scheduled(cron = "0 0 * * * *")
+    private void checkNewNews(){
+        Integer lastIdInDB = repository.findMaximumId();
+        int lastId = lastIdInDB == null ? 0 : lastIdInDB;
+        if (!parser.checkNewNewsExist(lastId)) {
+            System.out.println("parse");
+            List<News> news = parser.parse(lastId);
+            saveAllNews(news);
+        }
+    }
+
     @Scheduled(cron = "0 0 4 * * *")
-    private void checkNewsPhoto(){
+    private void checkNewsPhoto() {
         var allNews = getAllNews();
         List<News> crashedPhotoList = new ArrayList<>();
-        for (var news : allNews){
-            if (!checkNormalConnection(news.getPhoto())){
+        for (var news : allNews) {
+            if (!checkNormalConnection(news.getPhoto())) {
                 crashedPhotoList.add(news);
             }
         }
         var list = parser.parse();
-        for (var news : crashedPhotoList){
+        for (var news : crashedPhotoList) {
             var n = list.stream().filter(x -> x.getId() == news.getId()).findFirst().get();
-            if (n != null){
-                repository.updateNewsPhoto(n.getPhoto(),n.getId());
+            if (n != null) {
+                repository.updateNewsPhoto(n.getPhoto(), n.getId());
             }
         }
 
     }
-    private boolean checkNormalConnection(String path){
+
+    private boolean checkNormalConnection(String path) {
         int responseCode = 0;
-        try{
+        try {
             URL uri = new URL(path);
             var con = (HttpURLConnection) uri.openConnection();
             responseCode = con.getResponseCode();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return responseCode == 200;
