9c75219e19f0d87ffad243aeba88db0f139d1b10
Arseny <20mineslend14@gmail.com>
2022-08-25 19:29:24 +0500

    убран один из парсеров

--- user parser/config.py
+++
@@ -1,4 +0,0 @@
-db_host = 'ec2-52-49-120-150.eu-west-1.compute.amazonaws.com'
-db_dataBase = 'd19glhb8m2icr6'
-db_username = 'gymzhyrvnpeffa'
-db_password = '4008e77aaa744de70794011566f034bc937cac576e215b74953307613b45a489'
